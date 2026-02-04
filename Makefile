6f6b28b470056d0f536fa88a8d586fdf6b58dfb2
lethien <lethien7902@gmail.com>
2026-02-04 13:52:21 +0700

    ship with iso file

--- Makefile
+++ Makefile
@@ -17,5 +17,19 @@ mykernel.bin: linker.ld $(objects)
 install: mykernel.bin
 		sudo cp $< /boot/mykernel.bin
 
+mykernel.iso: mykernel.bin
+		mkdir iso
+		mkdir iso/boot
+		mkdir iso/boot/grub
+		cp $< iso/boot/
+		echo "set timeout=0" > iso/boot/grub/grub.cfg
+		echo "set default=0" >> iso/boot/grub/grub.cfg
+		echo "menuentry \"My Kernel\" {" >> iso/boot/grub/grub.cfg
+		echo "  multiboot /boot/mykernel.bin" >> iso/boot/grub/grub.cfg
+		echo "  boot" >> iso/boot/grub/grub.cfg
+		echo "}" >> iso/boot/grub/grub.cfg
+		grub-mkrescue -o $@ iso
+		rm -rf iso
 
-
+run: mykernel.iso
+		qemu-system-i386 -cdrom $<
---
+++ types.h
@@ -0,0 +1,25 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;       
+typedef signed short        int16_t;
+typedef signed int          int32_t;
+typedef signed long long    int64_t;
+typedef uint8_t             byte;
+typedef uint16_t            word;
+typedef uint32_t            dword;
+typedef uint64_t            qword; 
+typedef char                int8_t;
+typedef long long           int64_t;
+typedef int                 int32_t;
+typedef float               float32_t;
+typedef double              float64_t;
+typedef long double         float128_t;
+typedef uint8_t             bool_t;
+#define TRUE                1
+#define FALSE               0
+#define NULL                0
+#endif // TYPEDEF_H
